f8a4ab3af419d7c431c7eeb573238304c667a4bf
Connor Smith <connorsmith3034@hotmail.com>
2026-07-13 17:47:06 -0600

    created navagation bar

---
+++ Client/event_app/lib/login.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class Login extends StatefulWidget {
+  const Login({ Key? key }) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       
+    );
+  }
+}
